a86498a07bc8300cf3b5cf5e9be3c1a81e92b4b1
Brandon Lamb <brandon@brandonlamb.com>
2017-08-14 16:47:54 -0700

    checkpoint

--- src/example/main.gd
+++ src/example/main.gd
@@ -1,13 +1,13 @@
-extends Node2D
+extends Node
 
-onready var ip_address = $"ui/ip_address"
-onready var port = $"ui/port"
-onready var max_players = $"ui/max_players"
-onready var player_name = $"ui/player_name"
-onready var dedicated = $"ui/dedicated"
-onready var message = $"ui/message"
-onready var host = $"ui/host"
-onready var connect = $"ui/connect"
+onready var ip_address = $"ui/Control/ip_address"
+onready var port = $"ui/Control/port"
+onready var max_players = $"ui/Control/max_players"
+onready var player_name = $"ui/Control/player_name"
+onready var dedicated = $"ui/Control/dedicated"
+onready var message = $"ui/Control/message"
+onready var host = $"ui/Control/host"
+onready var connect = $"ui/Control/connect"
 
 func _ready():
 	gamestate.mainmenu = self
@@ -20,37 +20,37 @@ func _ready():
 			host_dedicated_server()
 
 func _host_pressed():
-	var name = player_name.get_text()
-	var port = port.get_text().to_int()
-	var maxcl = max_players.get_text().to_int()
+	var name = player_name.text
+	var p = port.text.to_int()
+	var maxcl = max_players.text.to_int()
 	var is_dedicated = dedicated.is_pressed()
 
 	gamestate.sv_dedicated = is_dedicated
 	gamestate.cl_name = name
 
-	if (!gamestate.host_game(port, maxcl)):
+	if (!gamestate.host_game(p, maxcl)):
 		return
 
 	set_control_enabled(false)
 
 func _connect_pressed():
-	var name = player_name.get_text()
-	var ip = ip_address.get_text()
-	var port = port.get_text().to_int()
+	var name = player_name.text
+	var ip = ip_address.text
+	var p = port.text.to_int()
 
-	if (!gamestate.join_game(ip, port)):
+	if (!gamestate.join_game(ip, p)):
 		return
 
 	gamestate.cl_name = name
 	set_control_enabled(false)
 
 func set_message(msg):
-	message.set_text(str(msg))
+	message.text = str(msg)
 
 func host_dedicated_server():
-	var name = name.get_text()
-	var port = port.get_text().to_int()
-	var maxcl = max_players.get_text().to_int()
+	var name = name.text
+	var port = port.text.to_int()
+	var maxcl = max_players.text.to_int()
 
 	gamestate.sv_dedicated = true
 	gamestate.cl_name = name
@@ -76,3 +76,14 @@ func set_control_enabled(value):
 #	connect.set_disabled(false)
 #
 #	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_resize_pressed():
+	#OS.set_window_size(Vector2(720, 320))
+	#get_viewport().set_size_override(true, Vector2(3840, 2160))
+	pass
+
+func _on_ui_scale_value_changed(value):
+
+	#get_node("ui/Control").rect_position = Vector2(3840/2, 2160/2)
+	get_node("ui/Control").set_global_position(Vector2(3840/2, 2160/2))
+	get_node("ui/Control").rect_scale = Vector2(value, value)
